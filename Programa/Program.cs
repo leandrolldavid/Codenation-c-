85d5ed3f66e1c8c6dfa3457c5076f0183f5a9764
leandrolldavid <leandrolldavid@gmail.com>
2020-06-02 13:25:47 -0300

    Update Program.cs

--- Programa/Program.cs
+++ Programa/Program.cs
@@ -9,7 +9,7 @@ namespace Programa
             LerArquico msg = new LerArquico();
             var mens = msg.LerArquivo();
 
-            Console.WriteLine($"Hello World! o Cryp é  e o Descryp é{mens} ");
+            Console.WriteLine($"Estou usando o git, Hello World! o Cryp é  e o Descryp é{mens} ");
         }
     }
 }
